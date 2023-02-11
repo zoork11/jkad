e95dc57669931f4873cbb6eaf30426488865719e
zoork <zoork@web.de>
2023-02-11 16:35:52 +0100

    removed google fonts

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
