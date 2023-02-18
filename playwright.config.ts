1c0cede54bef7addbf696ba9e78ec6697de5d92b
zoork <zoork@web.de>
2023-02-18 18:43:15 +0100

    removed port config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run serve',
-    port: 8080,
     url: 'http://localhost:8080',
     timeout: 120000,
   }
