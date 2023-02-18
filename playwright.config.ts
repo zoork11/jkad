8652e2235e13da3d117b938283ed31cdf0f7adbc
zoork <zoork@web.de>
2023-02-18 18:39:15 +0100

    checking with node version

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,10 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run serve',
+    port: 8080,
     url: 'http://localhost:8080',
     timeout: 120000,
-  },
+  }
 };
 
 export default config;
